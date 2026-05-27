c5d187744a3df1d16dea77471965e1eb8dc30716
C5DaB3st <csdabest@DESKTOP-GL2PMNJ.localdomain>
2026-05-26 21:49:56 -0500

    CAN Interface and setAngles

---
+++ byte_order.h
@@ -0,0 +1,7 @@
+#include <cstdint>
+
+void bufferAppendInt32(std::uint8_t *buffer, std::int32_t number,
+                       std::size_t &index);
+
+void bufferAppendInt16(std::uint8_t *buffer, std::int16_t number,
+                       std::size_t &index);
