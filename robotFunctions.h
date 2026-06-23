aa177d2f054389879fe1e513a8db04d69b02488d
5abah <54161308+5abah@users.noreply.github.com>
2026-06-23 00:52:26 -0500

    Update robotFunctions.h

--- robotFunctions.h
+++ robotFunctions.h
@@ -1,10 +1,13 @@
+#pragma once
+
 #include <array>
 #include <cstdint>
 
-class MotorJoint {
+class Can;
 
+class MotorJoint {
 public:
-  void setAngles(std::array<std::uint8_t, 6> motorID,
+  void setAngles(Can &can, std::array<std::uint8_t, 6> motorID,
                  std::array<float, 6> jointAngles,
                  std::array<float, 6> jointVel,
                  std::array<float, 6> jointAccel);
