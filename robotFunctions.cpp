d41b0e558efd3abaeb80929aadcb10107a50ad60
5abah <54161308+5abah@users.noreply.github.com>
2026-06-23 00:53:16 -0500

    Update robotFunctions.cpp

--- robotFunctions.cpp
+++ robotFunctions.cpp
@@ -1,11 +1,10 @@
 #include "robotFunctions.h"
 #include "can_interface.h"
 
-void MotorJoint::setAngles(std::array<std::uint8_t, 6> motorID,
-                           std::array<float, 6> jointAngles,
-                           std::array<float, 6> jointVel,
-                           std::array<float, 6> jointAccel) {
-  Can can;
+void MotorJoint::setAngles(Can &can, std::array<std::uint8_t, 6> motorID,
+                            std::array<float, 6> jointAngles,
+                            std::array<float, 6> jointVel,
+                            std::array<float, 6> jointAccel) {
   for (std::size_t i = 0; i < 6; i++) {
     can.encodeCAN(motorID.at(i), jointAngles.at(i), jointVel.at(i),
                   jointAccel.at(i), CAN_PACKET_ID::CAN_PACKET_SET_POS_SPD);
